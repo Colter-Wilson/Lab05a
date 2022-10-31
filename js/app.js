b5622bcb753d2e92253e3a3f81cb13b4059aa30b
Colter-Wilson <itscolter@gmail.com>
2022-10-31 00:11:42 -0400

    Question 5

--- js/app.js
+++ js/app.js
@@ -79,16 +79,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; 
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) {
+  let ogSum = sum(sumArr[0], sumArr[1])[0];
+  let arraySum = sum(ogSum [0], sumArr[2])[0];
 
+  let answerFour = [arraySum, `${sumArr} was passed in as an array of numbers, and ${arraySum} is their sum.`]
+
+  return answerFour;
 
 }
+ 
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -103,16 +109,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-  finalProduct = multiply(c, product)[0];
+// Write your code here
+ function multiplyArray(multArr) { //eslint-disable-line
+  let productOne = multiply(multArr[0], multArr[1])[0];
+   let productTwo = multiply(productOne, multArr[2])[0];
+ 
 
-  let answerFive = [finalProduct, `The numbers ${a}, ${b}, ${c} have a product of finalProduct`]
+   let answerFive = [productTwo, `The numbers ${multArr} have a product of ${productTwo}.`]
+ 
 
-  return answerFive;
+   return answerFive; }
 
 
 
-}
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
  testMultiplyArray(testArray);
